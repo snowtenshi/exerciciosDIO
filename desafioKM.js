8596bd7b40460cf963691d7db4f3acb3f5cdf1fb
Gabriela Mariano <gabrielamariano@live.com>
2024-01-31 20:55:08 -0300

    Reescreve código

--- desafioKM.js
+++ desafioKM.js
@@ -9,13 +9,13 @@ const valorViagem = precoCombustivel * litrosConsumidos
 console.log(valorViagem.toFixed(2)); 
 */
 
+const valorViagem = calcValorViagem(5.79, 50, 12);
+
 function calcValorViagem(precoCombustivel, distanciaPercorridaKM, kmPorLitros) {
-  const litrosConsumidos = distanciaPercorridaKM / kmPorLitros
-  return litrosConsumidos * precoCombustivel
-}
+  const litrosConsumidos = distanciaPercorridaKM / kmPorLitros;
+  const resultado = litrosConsumidos * precoCombustivel;
+  return `Valor gasto na viagem ${resultado.toFixed(2)}`;
+};
 
-let primeiraViagem = calcValorViagem(5.79, 50, 12)
-let segundaViagem = calcValorViagem(7.90, 100, 12)
-
-console.log(`O valor da primeira viagem foi: ${primeiraViagem.toFixed(2)} reais`)
-console.log(`O valor da segunda viagem foi: ${segundaViagem.toFixed(2)} reais`)
+console.log(valorViagem);
+console.log(calcValorViagem(7.90, 100, 12));
