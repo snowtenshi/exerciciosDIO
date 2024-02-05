93f9d400af87c3058c1db20611549f5b8812ddf9
Gabriela Mariano <gabrielamariano@live.com>
2024-02-05 19:15:46 -0300

    Adiciona método classificar IMC

--- exercicioPessoa.js
+++ exercicioPessoa.js
@@ -10,10 +10,25 @@ class Pessoa {
   }
 
   calcularIMC() {
-    return `Valor do IMC de ${this.nome} é ${(this.peso / Math.pow(this.altura, 2)).toFixed(1)}`;
+    return this.peso / Math.pow(this.altura, 2);
+  }
+
+  classificarIMC() {
+    const imc = this.calcularIMC()
+    if (imc < 18.5) {
+        return 'Abaixo do peso';
+    } else if (imc >= 18.5 && imc < 25) {
+        return 'Peso normal';
+    } else if (imc >= 25 && imc < 30) {
+        return 'Acima do peso';
+    } else if (imc >= 30 && imc < 40) {
+        return 'Obeso';
+    } else {
+        return 'Obsedidade Grave';
+    }
   }
 }
 
-const p1 = new Pessoa('José', 70, 1.75);
+const p1 = new Pessoa('José', 75, 1.75);
 console.log(p1);
-console.log(p1.calcularIMC());
+console.log(p1.classificarIMC());
